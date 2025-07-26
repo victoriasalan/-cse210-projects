65ccb0b99996d23f8cb77c55fd45b0d424371b1d
victoriasalan <victoriasalazarn@gmail.com>
2025-07-26 17:57:05 -0600

    Jorunal Program

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    public string GetRandomPrompt()
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+    };
+    
+}
