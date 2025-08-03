87e4964ffbeb2a703b02a3bf3fb94cb1fbe8133f
victoriasalan <victoriasalazarn@gmail.com>
2025-08-02 18:19:08 -0600

    updated

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Reference reference = new Reference("Proverbs", 3, 5, 6);
-        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding.";
+        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
         Scripture scripture = new Scripture(reference, text);
 
         while (true)
