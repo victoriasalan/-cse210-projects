60a485e03e39392e86da7f5486e1ca1c3dc4f83d
victoriasalan <victoriasalazarn@gmail.com>
2025-08-12 01:28:24 -0600

    homework practice

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Samuel Benett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "9-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
